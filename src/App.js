a9eb3dfde352a00f32f0dd2323b4d54d593f2536
Youssefanter <yooiialy@gmail.com>
2024-03-14 15:07:52 +0200

    Finish Setting up the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import {Article, Brand,CTA} from "./components";
+import React from "react";
 
+import {
+  Blog,
+  Features,
+  Header,
+  Possibility,
+  WhatGPT3,
+  Footer,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
